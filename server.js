b467f5d3c700a133d7a3dc262467a9fc11fa8cf8
Khanh <tran_khanh@outlook.com>
2020-07-09 14:04:08 +0700

    update server

--- server.js
+++ server.js
@@ -5,25 +5,15 @@
  * @date: 2019-10-10
  */
 
-var express = require("express");
-var app = express();
-var http = require("http");
-var port = "3030";
 
-const opn = require("opn");
-
+const express = require('express')
+const app = express()
+const port = 3000
 var compression = require("compression");
+
+
 app.use(compression());
 
 app.use(express.static("./build"));
-app.set("port", port);
 
-var server = http.createServer(app);
-server.listen(port);
-
-server.on("listening", onListening);
-
-function onListening() {
-	console.log(`server port 3118 listening and open browser with http://localhost:${port}....`);
-	opn(`http://localhost:${port}`, "chrome");
-}
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
