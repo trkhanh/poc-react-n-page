ff41903b8c4b1995ee97abab75580887ba726642
Khanh <tran_khanh@outlook.com>
2020-07-09 18:45:07 +0700

    update

--- src/component/footer.js
+++ src/component/footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<div className="footer main  column is-8">
-				<p className="text-center">powered by <a href="https://github.com/khanhtran/react-multi-page-app">@react-multi-page-app!</a></p>
+				<p className="text-center">powered by <a href="https://github.com/khanhtran/react-multi-page-app">@trkhanh8@gmail.com</a></p>
 			</div>
 		);
 	}
